679096718bb7508f0e7de1ba42b0a9f00192c666
IstvanM <istvan.meszaros.88@gmail.com>
2016-07-09 20:08:58 +0200

    Added first server-core spring boot based module (just a hello-world)

--- server-core/src/main/java/com/imeszaros/stockwatch/ServerCoreApplication.java
+++ server-core/src/main/java/com/imeszaros/stockwatch/ServerCoreApplication.java
@@ -1,5 +1,7 @@
 package com.imeszaros.stockwatch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,7 +9,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class ServerCoreApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(ServerCoreApplication.class);
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(ServerCoreApplication.class, args);
+        logger.info("Server core started up.");
     }
 }
